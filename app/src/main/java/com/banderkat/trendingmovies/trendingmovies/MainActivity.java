69efadadaab8a70efd8142b162f37b71fdd16e26
Kathryn Killebrew <banderkat@gmail.com>
2018-05-31 08:48:15 -0400

    Add grid view
    Add a grid layout with example content
    that scales a single test image to fill
    column width.

--- app/src/main/java/com/banderkat/trendingmovies/trendingmovies/MainActivity.java
+++ app/src/main/java/com/banderkat/trendingmovies/trendingmovies/MainActivity.java
@@ -2,12 +2,18 @@ package com.banderkat.trendingmovies.trendingmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
+
+    GridView gridView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gridView = findViewById(R.id.main_activity_gridview);
+        gridView.setAdapter(new MoviePosterAdapter(this));
     }
 }
---
+++ app/src/main/java/com/banderkat/trendingmovies/trendingmovies/MoviePosterAdapter.java
@@ -0,0 +1,84 @@
+package com.banderkat.trendingmovies.trendingmovies;
+
+import android.content.Context;
+import android.net.Uri;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+
+public class  MoviePosterAdapter extends BaseAdapter {
+
+    public static final String EXAMPLE_POSTER_URL = "http://image.tmdb.org/t/p/w185//nBNZadXqJSdt05SHLqgT0HuC5Gm.jpg";
+
+    private static final String LOG_LABEL = "PosterAdapter";
+
+    private final Context context;
+    private final LayoutInflater inflater;
+    private final Picasso picasso;
+
+    private static class ViewHolder {
+        ImageView imageView;
+    }
+
+    public MoviePosterAdapter(Context context) {
+        this.context = context;
+        this.inflater = LayoutInflater.from(context);
+
+        // configure Picasso with logging
+        Picasso.Builder builder = new Picasso.Builder(context);
+        builder.loggingEnabled(true);
+        builder.listener(new Picasso.Listener() {
+            @Override
+            public void onImageLoadFailed(Picasso picasso, Uri uri, Exception exception) {
+                Log.e(LOG_LABEL, "Failed to load image from " + uri.toString());
+                exception.printStackTrace();
+            }
+        });
+
+        picasso = builder.build();
+    }
+
+    @Override
+    public int getCount() {
+        return 12;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder;
+
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.poster_grid_item, parent, false);
+            viewHolder = new ViewHolder();
+            viewHolder.imageView = convertView.findViewById(R.id.poster_grid_item_image);
+
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        picasso.load(EXAMPLE_POSTER_URL).into(viewHolder.imageView);
+
+        // FIXME: load data and set description for image
+        viewHolder.imageView.setContentDescription("FIXME");
+
+        return convertView;
+    }
+}
